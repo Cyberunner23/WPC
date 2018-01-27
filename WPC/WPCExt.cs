5c097abcb6997c7fc51c6363c34a1c47a08cd7d1
Alex Frappier Lachapelle <Alex.FrappierLachapelle@gmail.com>
2018-01-27 12:13:15 -0500

    Basic plugin setup

---
+++ WPC/WPCExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using KeePass.Plugins;
+
+namespace WPC
+{
+    public class WPCExt : Plugin 
+    {
+
+        private IPluginHost _host = null;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            _host = host;
+            return true;
+        }
+    }
+}
