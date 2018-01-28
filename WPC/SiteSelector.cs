d49855ce43614594fe5a359b948e8dc7d0317394
Alex Frappier Lachapelle <Alex.FrappierLachapelle@gmail.com>
2018-01-28 03:52:46 -0500

    Some UI Tests

---
+++ WPC/SiteSelector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPC
+{
+    public class SiteSelector
+    {
+
+        private List<String> Scripts;
+
+        public SiteSelector()
+        {
+        }
+    }
+}
--- WPC/WPCExt.cs
+++ WPC/WPCExt.cs
@@ -3,6 +3,8 @@
 using KeePass.Plugins;
 using System.Windows.Forms;
 
+using KeePassLib.Security;
+
 namespace WPC
 {
     public class WPCExt : Plugin 
@@ -14,6 +16,29 @@ namespace WPC
         {
             MessageBox.Show("EYYY");
             _host = host;
+            var item = new ToolStripMenuItem()
+            {
+                Name = "AutoPasswordChange",
+                Text = "Auto Password Change"
+            };
+            item.DropDownItems.Add("GMail", null, (sender, e) => { MessageBox.Show("GMail"); });
+            item.DropDownItems.Add("Facebook", null, (sender, e) => { MessageBox.Show("Facebook"); });
+            item.DropDownItems.Add("Facebook", null, (sender, e) => {
+                Type type = sender.GetType();
+                MessageBox.Show($"TYPE: {type.Name}");
+                var pass = _host.MainWindow.GetSelectedEntry(false);
+                pass.Strings.Set("old_password", new ProtectedString().Insert(0, "asdfgh"));
+
+            });
+
+            _host.MainWindow.EntryContextMenu.Items.Add(item);
+
+            var current_db = _host.Database;
+            _host.MainWindow.SaveDatabase(current_db, this);
+
+            /*var pass = _host.MainWindow.GetSelectedEntry(false);
+            pass.Strings.Set("old_password", new ProtectedString().Insert(0, "asdfgh"));
+*/
             return true;
         }
     }
